553ec99bb3b3e3aed1c7a3bc5b972f425aab2007
muratjumashev <jumasheff@gmail.com>
2018-05-15 02:11:32 +0600

    Add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from authtools.models import AbstractEmailUser
 
-# Create your models here.
+
+class User(AbstractEmailUser):
+    full_name = models.CharField('full name', max_length=255, blank=True)
+
+    def get_full_name(self):
+        return self.full_name
+
+    def get_short_name(self):
+        return self.full_name
+
+    def __str__(self):
+        return '<{}>'.format(self.email)
+
+
+class Customer(models.Model):
+    photo = models.ImageField()
+    
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authtools',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 ROOT_URLCONF = 'config.urls'
 

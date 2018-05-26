ea6df554e66bdb49fc28849fc8cb1170cbf0effa
jumasheff <jumasheff@gmail.com>
2018-05-26 23:07:39 +0600

    Fix management command

--- apps/users/management/commands/move_profile_image.py
+++ apps/users/management/commands/move_profile_image.py
@@ -1,17 +1,21 @@
 import os
 import shutil
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management import BaseCommand
 
+DESTINATION = settings.MEDIA_ROOT
+
+def create_files_dir():
+    if not os.path.exists(DESTINATION):
+        os.makedirs(DESTINATION)
 
 class Command(BaseCommand):
-    help = "Copies profile.jpg to files/media/ folder"
+    help = "Command to copy profile.jpg to files/media"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         source = os.path.join(
             settings.BASE_DIR, 'apps', 'users', 'fixtures', 'profile.jpg')
-        desitination = settings.MEDIA_ROOT
-        shutil.copy(source, desitination)
-        message = "{} файлын {} папкасына көчүрдүк".format(
-            source, desitination)
+        create_files_dir()
+        shutil.copy(source, DESTINATION)
+        message = "{} файлын {} папкасына көчүрдүк".format(source, DESTINATION)
         self.stdout.write(self.style.SUCCESS(message)) # pylint: disable=E1101

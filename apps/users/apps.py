0088af7244a3b9ac9cb70ef73416110d775629a9
jumasheff <jumasheff@gmail.com>
2018-05-26 13:36:29 +0600

    Atomate management command calling

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,5 +1,11 @@
+import os
+from django.conf import settings
 from django.apps import AppConfig
+from django.core.management import call_command
 from django.db.models.signals import post_save
+
+
+PROFILE_IMAGE_IN_MEDIA = os.path.join(settings.MEDIA_ROOT, 'profile.jpg')
 
 
 class UsersConfig(AppConfig):
@@ -9,3 +15,5 @@ class UsersConfig(AppConfig):
         from .models import Employee
         from .signals import add_user_to_employees_group
         post_save.connect(add_user_to_employees_group, sender=Employee)
+        if not os.path.exists(PROFILE_IMAGE_IN_MEDIA):
+            call_command('move_profile_image')
